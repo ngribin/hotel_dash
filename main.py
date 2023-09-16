b81c9505df7dd9b4a2791f064f49c21263f29945
NIkolay Gribin <105112599+ngribin@users.noreply.github.com>
2023-09-17 01:04:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -250,6 +250,136 @@ elif selected_tab == "Темпы продаж":
 
 elif selected_tab == "Прогноз":
     st.subheader("На стадии обучения :)")
+    st.title("Forecast Tab")
+    st.write("This is the Forecast tab content.")
+
+    import streamlit as st
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from statsmodels.tsa.statespace.sarimax import SARIMAX
+    from statsmodels.tsa.arima_process import ArmaProcess
+    from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+    from statsmodels.tsa.stattools import adfuller
+    from sklearn.metrics import mean_squared_error
+
+    import warnings
+
+    warnings.filterwarnings('ignore')
+
+
+    # Load foot traffic data
+    data = pd.read_csv("foot_traffic.csv")
+
+    results = adfuller(data['foot_traffic'])
+    st.write('Statistic:', results[0])
+    st.write('P - value:', results[1])
+
+    alpha = 0.05
+
+
+    def ad_test(data):
+        results = adfuller(data)
+        if results[1] > alpha:
+            st.write('Time series is not stationary')
+        else:
+            st.write('We have stationary time series')
+            st.write(results[1])
+
+
+    ad_test(data['foot_traffic'])
+
+    foot_traffic_diff = np.diff(data['foot_traffic'], n=1)
+    ad_test(foot_traffic_diff)
+
+    plot_acf(foot_traffic_diff, lags=20)
+
+    plot_pacf(foot_traffic_diff, lags=20)
+    plt.tight_layout()
+
+    df_diff = pd.DataFrame({'foot_wear_diff': foot_traffic_diff})
+
+    train = df_diff[:-52]
+    test = df_diff[-52:]
+
+    TRAIN_LEN = len(train)
+    HORIZON = len(test)
+    WINDOW = st.slider("Select Window Size", min_value=1, max_value=5, value=1)
+
+
+    def rolling_forecast(df: pd.DataFrame, train_len: int, horizon: int,
+                         window: int, method: str) -> list:
+        total_len = train_len + horizon
+        end_idx = train_len
+
+        if method == 'mean':
+            pred_mean = []
+
+            for i in range(train_len, total_len, window):
+                mean = np.mean(df[:i].values)
+                pred_mean.extend(mean for _ in range(window))
+
+            return pred_mean
+
+        elif method == 'last':
+            pred_last_value = []
+            for i in range(train_len, total_len, window):
+                last_value = df[:i].iloc[-1].values[0]
+                pred_last_value.extend(last_value for _ in range(window))
+
+            return pred_last_value
+
+        elif method == 'AR':
+            pred_AR = []
+            for i in range(train_len, total_len, window):
+                model = SARIMAX(df[:i], order=(3, 0, 0))
+                res = model.fit(disp=False)
+                predictions = res.get_prediction(0, i + window - 1)
+                oos = predictions.predicted_mean.iloc[-window:]
+                pred_AR.extend(oos)
+
+            return pred_AR
+
+
+    pred_mean = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'mean')
+    pred_last_value = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'last')
+    pred_AR = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'AR')
+
+    test['pred_mean'] = pred_mean
+    test['pred_last_value'] = pred_last_value
+    test['pred_AR'] = pred_AR
+
+
+    st.write("Forecast Results:")
+    st.dataframe(test)
+
+    fig, ax = plt.subplots()
+    ax.plot(df_diff['foot_wear_diff'])
+    ax.plot(test['foot_wear_diff'], 'b-', label='actual')
+    ax.plot(test['pred_mean'], 'g:', label='mean')
+    ax.plot(test['pred_last_value'], 'r-.', label='last')
+    ax.plot(test['pred_AR'], 'k--', label='AR(3)')
+
+    ax.legend(loc=2)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Diff. avg. weekly foot traffic')
+    ax.axvspan(947, 998, color='#808080', alpha=0.2)
+    ax.set_xlim(920, 999)
+    ax.set_xticks([936, 988])
+    ax.set_xticklabels([2018, 2019])
+    fig.autofmt_xdate()
+    plt.tight_layout()
+
+    st.pyplot(fig)
+
+    mean_mse = mean_squared_error(test['foot_wear_diff'], test['pred_mean'])
+    last_mse = mean_squared_error(test['foot_wear_diff'], test['pred_last_value'])
+    ar_mse = mean_squared_error(test['foot_wear_diff'], test['pred_AR'])
+
+    st.write("Mean Squared Errors:")
+    st.write("Mean Forecast:", mean_mse)
+    st.write("Last Value Forecast:", last_mse)
+    st.write("AR(3) Forecast:", ar_mse)
     
 
 elif selected_tab == 'Рекомендации':
